7a7d260f7d9a5d48d61551d037fcbae7ce019863
Vlad21Islav <vmitk@mail.ru>
2023-06-13 14:42:37 +0300

    js code

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+const now = (date = new Date()) => date.getTime();
+
+const sleep = (interval) => {
+    interval *= 1000
+    const end = now() + interval;
+    while (now() < end);
+};
+
+const sum = (a, b) => a + b;
+
+for (const i of Object.keys(100)) {
+    var messageInput = document.getElementById('message');
+    var messageInput2 = document.getElementById('hello-message');
+    const amount = sum(messageInput.value, messageInput2.value);
+    console.log(amount, i);
+    sleep(5)
+};
