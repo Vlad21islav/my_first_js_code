d164502aa3804099e06a75b64ac0794d6fd053a6
Vlad21Islav <vmitk@mail.ru>
2023-06-15 13:57:45 +0300

    color

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ const color = (kind) => {
     };
   };
   
-  
-
-// const now = () => new Date().getTime();
-
-// const sleep = (interval) => {
-//     const interval_otv = interval * 1000;
-//     const end = now() + interval_otv;
-//     while (now() < end);
-// };
-
 const sum = (a, b) => a + ' ' + b;
 
 const messageInput = document.getElementById('message');
@@ -28,7 +18,7 @@ if (messageInput !== null && messageInput2 !== null) {
         setTimeout(() => {
             const amount = sum(messageInput.value, messageInput2.value);
             color(92)(amount + ' ' + i);
-        }, (1 * 1000));
+        }, ((100 - i) * (1 * 1000)));
     };
 }
 
